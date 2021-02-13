b3af67692da39be0390a63bbcd384dd6bced2612
astand <ast.and@bk.ru>
2021-02-14 00:18:39 +0300

    Added cmake building.

---
+++ bin/CMakeFiles/dbcscanner-lib.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/stone/source/cpp/qt/coderdbc-core/src/codegen/c-main-generator.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/codegen/c-main-generator.cpp.o"
+  "/home/stone/source/cpp/qt/coderdbc-core/src/codegen/c-sigprinter.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/codegen/c-sigprinter.cpp.o"
+  "/home/stone/source/cpp/qt/coderdbc-core/src/codegen/c-util-generator.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/codegen/c-util-generator.cpp.o"
+  "/home/stone/source/cpp/qt/coderdbc-core/src/codegen/conditional-tree.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/codegen/conditional-tree.cpp.o"
+  "/home/stone/source/cpp/qt/coderdbc-core/src/codegen/filewriter.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/codegen/filewriter.cpp.o"
+  "/home/stone/source/cpp/qt/coderdbc-core/src/codegen/fs-creator.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/codegen/fs-creator.cpp.o"
+  "/home/stone/source/cpp/qt/coderdbc-core/src/helpers/formatter.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/helpers/formatter.cpp.o"
+  "/home/stone/source/cpp/qt/coderdbc-core/src/parser/dbclineparser.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/parser/dbclineparser.cpp.o"
+  "/home/stone/source/cpp/qt/coderdbc-core/src/parser/dbcscanner.cpp" "/home/stone/source/cpp/qt/coderdbc-core/bin/CMakeFiles/dbcscanner-lib.dir/parser/dbcscanner.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DBCSCANNERLIB_LIBRARY"
+  "dbcscanner_lib_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/stone/source/cpp/qt/coderdbc-core/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
